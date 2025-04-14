788237549f05b7f27b954e62b17a65d3eedcb762
piyushpawar079 <piyushpawar079@gmail.com>
2025-04-14 15:47:39 +0530

    Reset to previous version of the cod e

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+    return (
+      <div className="text-center mt-20">
+        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        <p className="mt-2 text-gray-400">Sorry, we couldn't find that page.</p>
+      </div>
+    );
+  }
